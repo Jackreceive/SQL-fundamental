3e86b7b7a435cc3940ce5bab406b6e23794feca5
jack <jack@jackdeMacBook-Pro.local>
2026-09-04 13:25:27 +0800

    finish 04

--- main.sql
+++ main.sql
@@ -1,6 +1,8 @@
 CREATE TABLE cities (name TEXT, country TEXT);
 INSERT INTO cities VALUES ('Tokyo', 'Japan');
 INSERT INTO cities VALUES ('Paris', 'France');
+INSERT INTO cities VALUES ('Osaka', 'Japan');
+INSERT INTO cities VALUES ('Kyoto', 'Japan');
 INSERT INTO cities VALUES ('Cairo', 'Egypt');
 
-SELECT name FROM cities;
+SELECT name FROM cities WHERE country='Japan'
